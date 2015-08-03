7eeae05f6197b998651309a3b6111c7600af18b6
diego-rc <diego.rc.91@gmail.com>
2015-08-03 10:01:15 -0600

    Selection Sort
    Lo mas facil es que todos los sorts sean un metodo estatico dentro de la
    clase sorts

--- Hoja3/src/hoja3/Sort.java
+++ Hoja3/src/hoja3/Sort.java
@@ -11,10 +11,20 @@ import java.util.ArrayList;
  */
 public class Sort {
     
-    public static ArrayList<Comparable> SelectionSort(ArrayList<Comparable[]> unsorted){
-        ArrayList<Comparable[]> sorted;
+    public static ArrayList<Comparable> SelectionSort(ArrayList<Comparable> unsorted){
+        ArrayList<Comparable> sorted = new ArrayList<>();
         for(int i=0; i<unsorted.size(); i++){
-            
+            Comparable menor=unsorted.get(0);
+            int index=0;
+            //este ciclo encuentra el menor que contiene unsorted
+            for(int j=1;i<unsorted.size(); j++){
+                if(menor.compareTo(unsorted.get(j))<0){
+                    menor=unsorted.get(j);
+                    index=j;
+                }
+            }
+            sorted.add(0, menor);
+            unsorted.remove(index);
         }
         return sorted;
     }
