65ae0872b1067e13434c9bf07b65e1c8024ab545
diego-rc <diego.rc.91@gmail.com>
2015-08-03 09:45:53 -0600

    Primer codigo

---
+++ Hoja3/src/hoja3/Sort.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoja3;
+import java.util.ArrayList;
+/**
+ *
+ * @author diego_000
+ */
+public class Sort {
+    
+    public static ArrayList<Comparable> SelectionSort(ArrayList<Comparable[]> unsorted){
+        ArrayList<Comparable[]> sorted;
+        for(int i=0; i<unsorted.size(); i++){
+            
+        }
+        return sorted;
+    }
+}
